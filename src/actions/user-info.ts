35ac100e57d258fd166199d8e148b8c2a8967a30
carlos <carlos8189.mk@gmail.com>
2024-11-18 17:22:58 -0300

    feat: edited server action that pulls user data, implemented new security checks

--- src/actions/user-info.ts
+++ src/actions/user-info.ts
@@ -1,21 +1,34 @@
 'use server';
-import { db } from '@/lib/prisma';
+
 import { cookies } from 'next/headers';
 import jwt from 'jsonwebtoken';
+import { db } from '@/lib/prisma';
 
-export default async function userInfo() {
-  const token = (await cookies()).get('token')?.value as string;
-  const decode = jwt.decode(token) as { id: string };
+export async function getUser() {
+  const token = (await cookies()).get('token')?.value;
+  const currentTime = Math.floor(Date.now() / 1000);
+  const decode = token && jwt.decode(token);
 
-  const user = await db.user.findMany({
-    where: {
-      id: decode.id,
-    },
-    select: {
-      name: true,
-      email: true,
-    },
-  });
+  if (!token) {
+    return undefined;
+  }
 
-  return user[0];
+  if (decode instanceof Object && decode.exp && decode.exp < currentTime) {
+    return undefined;
+  }
+
+  if (decode instanceof Object && decode.exp && decode.exp > currentTime) {
+    const user = await db.user.findMany({
+      select: {
+        email: true,
+        name: true,
+        id: true,
+      },
+      where: {
+        id: decode.id,
+      },
+    });
+
+    return user[0];
+  }
 }
