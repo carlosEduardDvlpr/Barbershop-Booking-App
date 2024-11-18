612c3b2dbbb1dd238cf845be93effdf71a7aa177
carlos <carlos8189.mk@gmail.com>
2024-11-17 22:22:00 -0300

    feat: added server action that registers user

---
+++ src/actions/user-register.ts
@@ -0,0 +1,44 @@
+'use server';
+import { db } from '@/lib/prisma';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { cookies } from 'next/headers';
+
+export default async function userRegister(
+  name: string,
+  email: string,
+  password: string,
+) {
+  const hash = bcrypt.hashSync(password, 12);
+
+  try {
+    const user = await db.user.create({
+      data: {
+        name,
+        email,
+        password: hash,
+      },
+    });
+    (await cookies()).set(
+      'token',
+      jwt.sign({ id: user.id }, process.env.SECRET as string, {
+        expiresIn: '4d',
+      }),
+      {
+        httpOnly: true,
+        secure: true,
+      },
+    );
+
+    return { success: true, error: '' };
+  } catch (error) {
+    if (
+      error instanceof PrismaClientKnownRequestError &&
+      error.code === 'P2002'
+    ) {
+      return { success: false, error: 'O email já existe!' };
+    }
+    return { success: false, error: 'Ocorreu um erro, tente novamente...' };
+  }
+}
