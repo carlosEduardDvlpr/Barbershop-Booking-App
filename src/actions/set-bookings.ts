21b11bfdea31755a0d5e54e8dc82fb48d5f770c0
carlos <carlos8189.mk@gmail.com>
2024-11-24 17:21:48 -0300

    fix:adjusts date

--- src/actions/set-bookings.ts
+++ src/actions/set-bookings.ts
@@ -10,7 +10,7 @@ export async function setBooking(date: Date, userId: string) {
   try {
     await db.booking.create({
       data: {
-        date,
+        date: new Date(date),
         userId,
       },
     });
