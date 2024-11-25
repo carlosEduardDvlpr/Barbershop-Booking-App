3936d9ba11273b2d1aae4908e53e70b370195eb7
carlos <carlos8189.mk@gmail.com>
2024-11-25 17:18:21 -0300

    date

--- src/actions/set-bookings.ts
+++ src/actions/set-bookings.ts
@@ -10,7 +10,7 @@ export async function setBooking(date: Date, userId: string) {
   try {
     await db.booking.create({
       data: {
-        date: new Date(date).toString(),
+        date,
         userId,
       },
     });
